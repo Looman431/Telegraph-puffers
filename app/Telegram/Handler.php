8bd9f3b0b355b8b763ce0559cc0f601ef2eaca88
Looman431 <looman431@gmail.com>
2025-08-02 11:34:59 +0300

    Теперь начинаем писать самого бота

---
+++ app/Telegram/Handler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Telegram;
+
+use DefStudio\Telegraph\Handlers\WebhookHandler;
+
+class Handler extends WebhookHandler
+{
+    public function start(): void{
+        $this->reply('Бот работает вроде как');
+    }
+}
